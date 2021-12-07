04b32d68fa554f5dde5f22c290b79199d98043de
Killian Blais <killian.blais@gmail.com>
2021-12-07 14:31:38 +0700

    - low cpu usage from sandbox works

--- src/index.js
+++ src/index.js
@@ -1,58 +1,47 @@
-import {
-    Viewer,
-    Cartesian3,
-    CesiumTerrainProvider,
-    TileCoordinatesImageryProvider,
-    WebMapTileServiceImageryProvider,
-} from "cesium";
+import * as Cesium from "cesium"
 
 // import Cesium from "cesium";
 import "cesium/Widgets/widgets.css";
 import "../src/css/main.css"
 
-var viewer = new Viewer("cesiumContainer", {
-    requestRenderMode : true,
-    maximumRenderTimeChange : Infinity,
-    baseLayerPicker: false,
-    terrainProvider : new CesiumTerrainProvider({
-        url : 'http://localhost:4000/tilesets/srtm/' ,
-        requestVertexNormals : true
-    }),
-    // timeline: false,
-    // animation : false,
-    showRenderLoopErrors: true
-});
-
-var layers = viewer.scene.imageryLayers;
-layers.removeAll();
+// Create a viewer that won't render a new frame unless
+// updates to the scene require it to reduce overall CPU usage.
+var viewer = new Cesium.Viewer("cesiumContainer", {
+    requestRenderMode: true,
+    maximumRenderTimeChange: Infinity,
+    terrainProvider: Cesium.createWorldTerrain(),
+  });
 
 var scene = viewer.scene;
 scene.debugShowFramesPerSecond = true;
 
+var tileset;
 
-// keep zoom level increment whole
-// cf: https://groups.google.com/g/cesium-dev/c/eBgIKjw6HCE/m/XQEfZIZXWIwJ
-viewer.scene.globe.maximumScreenSpaceError = 1;
+var viewModel = {
+    requestRenderMode: true,
+    showTimeOptions: false,
+    timeChangeEnabled: false,
+    maximumRenderTimeChange: 0.0,
+    lastRenderTime: "",
+    requestRender: function () {
+        scene.requestRender();
+    },
+};
 
-var high_res = layers.addImageryProvider(new WebMapTileServiceImageryProvider({
-    url : 'http://127.0.0.1:5000/qgis_generate/WMTS',
-    layer : 'qgis_generate',
-    style: 'default',
-    format : 'image/jpg',
-    tileMatrixSetID : 'GoogleMapsCompatible',
-    maximumLevel: 14
-}));
-
-var tile_layer = layers.addImageryProvider(
-    new TileCoordinatesImageryProvider()
-);
-
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-viewer.camera.flyTo({
-    destination : Cartesian3.fromDegrees(41.59, 41.61, 9000),
-    // orientation : {
-    //   heading : Math.toRadians(0.0),
-    //   pitch : Math.toRadians(-15.0),
-    // }
-  });
+// Clear scene and set default view.
+var handler;
+function resetScene() {
+    viewer.trackedEntity = undefined;
+    viewer.dataSources.removeAll();
+    viewer.entities.removeAll();
+    viewer.scene.primitives.remove(tileset);
+    viewer.clock.shouldAnimate = false;
+    handler = handler && handler.destroy();
+    scene.skyBox.show = true;
+    scene.camera.flyHome(0.0);
+    scene.requestRender();
+    viewModel.showTimeOptions = false;
+    viewModel.timeChangeEnabled = false;
+    viewModel.maximumRenderTimeChange = 0;
+}
 
